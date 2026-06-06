4abe5386c79626611bd4eb94d57ec4aab79fd1aa
Sourabh108-Coder <kumarsourabh7164@gmail.com>
2026-06-06 19:23:12 +0530

    Fix Tab Name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar"; 
 import Footer from "@/components/home/Footer";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return(
     <>
+       <Head>
+        <title>Arogya Sadhana</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />;
       <Footer/>
